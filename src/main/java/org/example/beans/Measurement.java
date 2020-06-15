dbeb3fee338e854a7c736492d6ffba99fed401fc
Riccardo Graziosi <riccardo.graziosi97@gmail.com>
2020-06-15 17:59:50 +0200

    Add id and type fields to Measurement

--- src/main/java/org/example/beans/Measurement.java
+++ src/main/java/org/example/beans/Measurement.java
@@ -4,15 +4,27 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class Measurement {
+    private String id;
+    private String type;
     private double value;
     private long timestamp;
 
     public Measurement() {}
 
-    public Measurement(double value, long timestamp) {
+    public Measurement(String id, String type, double value, long timestamp) {
+        this.id = id;
+        this.type = type;
         this.value = value;
         this.timestamp = timestamp;
     }
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
+
+    public String getType() { return type; }
+
+    public void setType(String type) { this.type = type; }
 
     public double getValue() {
         return value;
